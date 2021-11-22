c0d2f09b835e5d8d0e935b60f3da9278b62cd369
JustinProoo <justincomans.2003@gmail.com>
2021-11-22 09:15:35 +0100

    added Logs to Gitignore

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -13,12 +13,27 @@ public class GridManager : MonoBehaviour
     {
         none = 0
     }
+    [Header("Grid")]
+    [SerializeField] Vector2Int gridSize;
     public tileTypes[,] grid { get; set; }
 
-    [SerializeField] Vector2Int gridSize;
+    [Header("Variables")]
+    [SerializeField] int tileSize = 3;
     private void Start()
     {
         grid = new tileTypes[gridSize.x, gridSize.y];
-        Debug.Log(grid[gridSize.x - 1, gridSize.y - 1]);
     }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        for (int X = 0; X < gridSize.x; X++)
+        {
+            for (int Y = 0; Y < gridSize.y; Y++)
+            {
+                Gizmos.DrawWireCube(new Vector3(tileSize * X / 2, 0, tileSize * Y / 2), new Vector3(tileSize, 0 , tileSize));
+            }
+        }
+    }
+#endif
 }
